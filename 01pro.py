f855723e8f2112c7a2ee85d98c529f08b3ea9772
MEENAKSHI PALAI <meenakshipalai368@gmail.com>
2024-07-16 15:48:40 +0530

    Define Variables

--- 01pro.py
+++ 01pro.py
@@ -1,2 +1,14 @@
-#print first program
-print("Hello World")
+print(21)
+print(21+35)
+#Define Variables
+name = "Meenakshi"#string
+age = 21
+
+print(name)
+print(age)
+
+print("my name is :" , name)
+print("my age is :" , age)
+
+age2 = age
+print(age2)
