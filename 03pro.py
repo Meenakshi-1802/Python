7808c7a0d3d7b551f99add9231a92f53c3d71969
MEENAKSHI PALAI <meenakshipalai368@gmail.com>
2024-07-16 20:14:15 +0530

    data types

---
+++ 03pro.py
@@ -0,0 +1,25 @@
+#identifiers
+name = "Meenakshi"
+age = 21
+price = 25.99
+
+print(type(name))
+print(type(age))
+print(type(price))
+
+#string
+name1 = 'python'
+name2 = "python"
+name3 = '''python'''
+
+print(name1)
+print(name2)
+print(name3)
+
+#boolean,none
+age = 21
+old = False
+a = None
+
+print(type(old))
+print(type(a))
