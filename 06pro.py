e9129ddb1ca0206a96b8d326756b3f0f310e555d
MEENAKSHI PALAI <meenakshipalai368@gmail.com>
2024-07-17 11:47:53 +0530

    Print output

---
+++ 06pro.py
@@ -0,0 +1,24 @@
+#single line comment
+#Meenakshi's code
+"""This is
+ a multi line
+   comment"""
+
+# print input for string value
+name = input("name :")
+print(name)
+
+#print input for int value
+age = int(input("age : "))
+print(age)
+
+#print input for float value
+price = float(input("price:"))
+print(price)
+
+print("My name is" , name ,"and i am" ,age ,"years old")
+
+
+
+
+
