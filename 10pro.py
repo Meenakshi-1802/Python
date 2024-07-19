9d911c92e83a9da8e97f21a00f272fbfb584df4f
MEENAKSHI PALAI <meenakshipalai368@gmail.com>
2024-07-19 19:50:00 +0530

    Operators

---
+++ 10pro.py
@@ -0,0 +1,36 @@
+#arithmatic operators
+a = 5
+b = 2
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)#remainder
+print(a ** b)#a^b
+
+#Relational / Comparison operator
+a = 50
+b = 20
+print(a == b)#False
+print(a != b)#True
+print(a >= b)#True
+print(a > b)#True
+print(a <= b)#False
+print(a < b)#False
+
+#Assignment operator
+num = 10
+num = num + 10 #10+10 => 20
+print(num)
+
+#logical operator
+print(not False)
+print(not True)
+val1 = True
+val2 = True
+print("AND operator:" , val1 and val2)
+print("OR operator:" , val1 or val2)
+
+
+
+
