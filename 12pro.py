73f241f47ab5f6de8d39c7956e498e33845547a4
MEENAKSHI PALAI <meenakshipalai368@gmail.com>
2024-07-23 19:35:56 +0530

    Input in python

---
+++ 12pro.py
@@ -0,0 +1,14 @@
+#input in python
+name = input("enter your name :")
+print("welcome" , name)
+
+val = input("enter some value : ")
+print(type(val) , val)
+
+name = input("enter name: ")
+age = input("enter age: ")
+marks = input("enter marks: ")
+
+print("welcome" , name)
+print("age =" , age)
+print("marks =" , marks)
