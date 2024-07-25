c4a96b9fa95559639bdcd4dd64a8629af0a32cf5
MEENAKSHI PALAI <meenakshipalai368@gmail.com>
2024-07-25 19:29:28 +0530

    Tuples

---
+++ 16pro.py
@@ -0,0 +1,20 @@
+#Tuples
+tup = (2,1,3,4,)
+print(tup[0])
+print(tup[1])
+tup[0] = 5
+
+#tuple slicing
+tup = (1,2,3,4)
+print(tup[1:3])
+
+tup = (1,2,3,4)
+print(tup.index(2))
+
+tup = (1,2,3,4)
+print(tup.count(3))
+
+
+
+
+
