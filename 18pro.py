d0ca1274b962f265a7a20a57a9742f1bae27c418
MEENAKSHI PALAI <meenakshipalai368@gmail.com>
2024-07-26 13:42:39 +0530

    Sets

---
+++ 18pro.py
@@ -0,0 +1,37 @@
+#sets
+collection = {1,2,3,4}
+print(collection)
+print(type(collection))
+print(len(collection))
+
+collection = set() #empty set; syntax
+
+print(type(collection))
+
+collection = set()
+collection.add(1)
+collection.add(2)
+collection.add(3)
+collection.remove(3)
+collection.clear
+print(collection)
+print(len(collection))
+
+#union of sets
+set1 = {1,2,3}
+set2 = {2,3,4}
+print(set1.union(set2))
+print(set1)
+print(set2)
+
+#intersection of sets
+set1 = {1,2,3}
+set2 = {2,3,4}
+print(set1.intersection(set2))
+print(set1)
+print(set2)
+
+
+
+
+
