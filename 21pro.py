ce2a798044a7751193b6efc9462483d4a8bc44c7
MEENAKSHI PALAI <meenakshipalai368@gmail.com>
2024-07-28 19:54:25 +0530

    functions

---
+++ 21pro.py
@@ -0,0 +1,25 @@
+#Functions Definition 
+def calc_sum(a,b): #parameters
+    sum = a + b
+    print(sum)
+    return sum
+
+calc_sum(5,10) #function call ; arguments
+#more lines of code
+calc_sum(2,10)
+
+calc_sum(12,17)
+
+#average of 3 numbers
+def calc_avg(a,b,c):
+    sum = a + b + c
+    avg = sum / 3
+    print(avg)
+    return avg
+calc_avg(1,2,3)
+
+#types
+print("meenakshi" ,"python")
+print("meenakshipalai",end=" ")
+print("python is a programming language")
+
