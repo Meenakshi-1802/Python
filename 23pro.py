c8b46d056a5371ce8266dd10a7c923d6cfe98d8e
MEENAKSHI PALAI <meenakshipalai368@gmail.com>
2024-07-29 21:30:08 +0530

    Revision

---
+++ 23pro.py
@@ -0,0 +1,26 @@
+#Revision
+#ques-1
+number = int(input("enter a number: "))
+
+if number >= 0:
+    print("the number is positive")
+else:
+    print("the number is negative")
+
+#ques-2
+for i in range(1,11):
+    print("i,python")
+
+#ques-3
+list = [1,2,3,4,5,6]
+fruits = ["apple","banana","kiwi"]
+for i in list:
+    print(i)
+for i in fruits:
+    print(i)
+
+#ques-4
+i = 2
+while i < 101:
+    print(i)
+    i += 2
