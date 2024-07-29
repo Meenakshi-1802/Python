37df8a06c62d3a777af7febd5b3b2e404b62fe2e
MEENAKSHI PALAI <meenakshipalai368@gmail.com>
2024-07-29 11:59:23 +0530

    21pro.added

--- 21pro.py
+++ 21pro.py
@@ -19,7 +19,37 @@ def calc_avg(a,b,c):
 calc_avg(1,2,3)
 
 #types
-print("meenakshi" ,"python")
-print("meenakshipalai",end=" ")
+print("meenakshi" ,"python") #sep = " "
+print("meenakshipalai",end=" ") 
 print("python is a programming language")
+
+#quest-1
+cities = ["delhi" , "gurgaon" , "pune" , "noida"]
+heros = {"thor" , "ironman" , "spiderman" ,"batman"}
+print(heros[0],end=" ")
+print(heros[1],end=" ")
+def print_len(list):
+    print(len(list))
+print_len(cities)
+print_len(heros)
+
+def print_list(list):
+    for item in list:
+        print(item,end=" ")
+print_list(heros)
+print()
+
+#ques-2
+n = 5
+fact = 1
+for i in range(1, n+1):
+    fact *= i
+print(fact)
+
+def converter(usd_val):
+    inr_val = usd_val * 83
+    print(usd_val,"USD =" , inr_val, "INR")
+
+    converter(73)
+
 
