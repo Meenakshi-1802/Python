18addfb25bbecdf21c3f0631f7e5541bfe0ae9b3
MEENAKSHI PALAI <meenakshipalai368@gmail.com>
2024-08-01 16:52:00 +0530

    revision added

--- 23pro.py
+++ 23pro.py
@@ -24,3 +24,27 @@ i = 2
 while i < 101:
     print(i)
     i += 2
+
+#conversion
+x = 11 # int
+y = 2.88 # float
+z = 1j # complex
+
+#convert from int to float:
+a = float(x)
+
+#convert from float to int:
+b = int(y)
+
+#convert from int to complex:
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+
+
+print(type(a))
+print(type(b))
+print(type(c))
+
