8038781b57b66139f9aa04a3d703999cfd12fe61
MEENAKSHI PALAI <meenakshipalai368@gmail.com>
2024-08-17 19:12:57 +0530

    iterative statements

---
+++ 33pro.py
@@ -0,0 +1,24 @@
+#iterative statements
+h = "vishal"
+for k in h:
+    print("hello")
+
+#range
+for i in range (5):
+    print("hello")
+
+y = range(5,7)
+for i in y:
+    print(i) 
+
+z = range(5,15,3)
+for i in z:
+    #print(i) 
+
+x = [11,22,33,44,55,66,77,88]
+for i in x:
+    print(i)
+y = "raju" 
+for i in y:
+    print(i) 
+
