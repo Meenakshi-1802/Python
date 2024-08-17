02f90a2a600f07301b52b73995d41c9987fec5b3
MEENAKSHI PALAI <meenakshipalai368@gmail.com>
2024-08-17 19:40:43 +0530

    swapping

---
+++ 35pro.py
@@ -0,0 +1,20 @@
+#swapping
+x = int(input("enter first number:"))
+y = int(input("enter second number:"))
+
+temp = x
+x = y
+y = temp
+
+print(x,y)
+
+x = int(input("enter first number:"))
+y = int(input("enter second number:"))
+print("x =",x,"y =",y, "sep =")
+      
+temp = x
+x = y
+y = temp
+print("after swapping:")
+print("x =",x,   "y =",y, "12sep =")
+
