8e6336e1b8762fbd511e84c2a799cb3f27c07a1c
MEENAKSHI PALAI <meenakshipalai368@gmail.com>
2024-08-19 16:18:37 +0530

    32pro added

--- 32pro.py
+++ 32pro.py
@@ -5,6 +5,12 @@ if x>0:
 else:
     print(x,"is negative")
 
+age = int(input("how old are you ?:"))
+if age >= 18:
+    print("you are an adult !")
+else:
+    print("you are a child !")
+
 #find maximum of 2 numbers
 p = int(input("enter first number:"))
 s = int(input("enter second number:"))
