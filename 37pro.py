931cf0e159bf477915058427deac0cf9ff88404c
MEENAKSHI PALAI <meenakshipalai368@gmail.com>
2024-08-19 14:22:50 +0530

    added

---
+++ 37pro.py
@@ -0,0 +1,15 @@
+#math functions
+pi = 3.14
+print(round(pi))
+print(math.ceil(pi))
+print(math.floor(pi))
+print(abs(pi))
+print(pow(pi,2))
+print(math.srt(pi))
+
+pi = 3.14
+x = 1
+y = 2
+z = 3
+print(max(x,y,z))
+print(min(x,y,z))
