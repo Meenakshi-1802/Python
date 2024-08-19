3a6863a9b54ae1e39903c2eee169c72c1ebfcc1c
MEENAKSHI PALAI <meenakshipalai368@gmail.com>
2024-08-19 12:32:14 +0530

    added

---
+++ 36pro.py
@@ -0,0 +1,10 @@
+#methods of strings
+name = "meenakshi palai"
+print(len(name))
+print(name.find("s"))
+print(name.capitalize())#only capitalize first letter of the name
+print(name.upper())#capitalize whole name
+print(name.lower())
+print(name.count("e"))
+print(name.replace("k","g"))
+print(name*4)
