9b536a9774ea80f6656685d7c7f1c94e67918e3a
MEENAKSHI PALAI <meenakshipalai368@gmail.com>
2024-08-24 18:14:05 +0530

    added

---
+++ 51pro.py
@@ -0,0 +1,51 @@
+#(11)
+width = float(input("enter the width of the rectangle:"))
+height = float(input("enter the height of the rectangle:"))
+area = width*height
+print("the area of the rectangle is",area)
+
+#(12)
+my_list = [1,2,3,4,5,]
+print(my_list)
+
+#(13)
+my_list = ["pizza","burger","coke"]
+print(my_list[2])
+
+#(14)
+num1 = 300
+num2  = 200
+num3 = 100
+largest = max(num1,num2,num3)
+print("the largest num is",largest)
+
+#(15)
+my_list = [1,2,3,4,5]
+average = sum(my_list)/5
+print("the average of 5 numbers is",average)
+
+#(16)
+numbers = [1,2,3,4,5,6,7,8,9,10]
+for i in numbers:
+    print(i)
+
+#(17)
+numbers = [1,2,3,4,5]
+for i in numbers:
+    print(i)
+
+#(18)
+#i = 5
+while i >= 1 :
+    print(i)
+    i -= 1
+
+#(19)
+num = int(input("enter a number:"))
+for i in range(1,11):
+    print(num*i)
+
+#(20)
+
+characters = "meenakshi"
+print(len(characters))
